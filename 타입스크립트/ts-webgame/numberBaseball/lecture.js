7fbb05863d5a748d71dac1dd0b442bee8c2bc853
LeeJiHo <easyho93@gmail.com>
2021-04-12 22:25:22 +0900

    typescript webgame

--- 타입스크립트/ts-webgame/numberBaseball/lecture.js
+++ 타입스크립트/ts-webgame/numberBaseball/lecture.js
@@ -1,8 +1,10 @@
 var num = 3;
-var str;
-var arr = [1, 2, 3];
-var arr2 = [1, 2, 'str'];
-var arr3 = [true, '2', 3];
+var str = num.toString();
+var arr = [1, 2, 3, 'a'];
+var arr2 = [1, 2, 3];
+var arr3 = [1, 2, 'str'];
+var arr4 = [true, '2', 3]; // readonly
+var ob = { a: 'b' };
 var a = 1;
 var Color;
 (function (Color) {
@@ -12,3 +14,17 @@ var Color;
 })(Color || (Color = {}));
 ;
 var c = Color.Green;
+function add(a, b) {
+    return a + b;
+}
+function f(a, b) {
+    return function (s) {
+        console.log(s);
+        return a + b;
+    };
+}
+var obj = {
+    a: function (b) {
+        return 'hi';
+    }
+};
--- 타입스크립트/ts-webgame/numberBaseball/lecture.ts
+++ 타입스크립트/ts-webgame/numberBaseball/lecture.ts
@@ -1,15 +1,18 @@
 let num: number =  3;
 
-let str: string;
 
-let arr: (string | number)[] = [1, 2 ,3];
+let str: string = num.toString();
 
-let arr2: [number, number, string] = [1, 2, 'str'];
+let arr: (string | number)[] = [1, 2 ,3, 'a'];
+let arr2: Array<number | string> = [1, 2, 3];
 
-let arr3 = [true, '2', 3] as const;
+let arr3: [number, number, string] = [1, 2, 'str'];
+
+let arr4 = [true, '2', 3] as const; // readonly
+
+const ob: { a: string, b?: number } = { a: 'b'};
 
 const a = 1;
-
 
 enum Color { Red, Green, Blue};
 let c: Color = Color.Green
@@ -25,9 +28,17 @@ function f(a: number, b: number): (c: string) => number {
     }
 }
 
-const obj: { a?: (b: number) => String } = {
-    a(b: number) {
+const obj: { a?: (b?: number) => String } = {
+    a(b?: number) {
         return 'hi';
     }
 }
+
+const hi: any = false // 자바스크립트 = any
+
+const hello: number = 3;
+(hello as unknown as string).substr(1, 3);
+(<string><unknown>hello).substr(1, 3);
  
+const div = document.createElement('div');
+const abc = div as HTMLElement    // 부모 자식 관계라 unknown 필요없음
